6cb68d40bbd5592d96c04631d08a2c24f9d676bf
Truls Johan Jacobsen-Lamøy <truls@Mac.home>
2025-04-17 20:08:00 +0200

    chore: added constants

---
+++ src/hooks/auth/login.jsx
@@ -0,0 +1,8 @@
+export async function login() {
+    try {
+        const res = await fetch()
+        const data = res.json();
+    } catch(error) {
+
+    }
+}
---
+++ src/utility/constants.jsx
@@ -0,0 +1,4 @@
+export const API_BASE = 'https://v2.api.noroff.dev';
+export const API_AUTH = `${API_BASE}/auth`;
+export const API_REGISTER = `${API_AUTH}/register`;
+export const API_LOGIN = `${API_AUTH}/login`;
