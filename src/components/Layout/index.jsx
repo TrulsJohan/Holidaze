de8a5a282d40f44e64f40b8d2e8dfe831c6fa481
Truls Johan Jacobsen-Lamøy <truls@Mac.home>
2025-04-21 16:42:26 +0200

    chore: added a header for mobile with menu

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,15 +1,13 @@
-import { Outlet } from "react-router-dom";
-import { Background } from "./Background";
-import { Header } from "./Header";
-import { Footer } from "./Footer";
+import { Outlet } from 'react-router-dom';
+import { Header } from './Header';
+import { Footer } from './Footer';
 
 export const Layout = () => (
-    <>
+    <div className="flex flex-col min-h-screen">
         <Header />
-        <Background />
-        <main className="grow">
+        <main className="grow z-30 relative">
             <Outlet />
         </main>
-        <Footer />
-    </>
+        <Footer className="z-30" />
+    </div>
 );
