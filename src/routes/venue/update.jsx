ac4896a7d229db324f9b15e57ea57db43605cb31
Truls Johan Jacobsen-Lamøy <truls@Mac.home>
2025-05-20 22:04:14 +0200

    chore: made update venue responsive

--- src/routes/venue/update.jsx
+++ src/routes/venue/update.jsx
@@ -4,23 +4,23 @@ import { useNavigate } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 
 export function RenderUpdateVenue() {
-    const { id } = useParams(); 
+    const { id } = useParams();
     const navigate = useNavigate();
     const handleBack = () => {
         navigate(`/venue/${id}`);
-    }
+    };
 
     return (
-        <div className="min-h-screen px-8 py-12">
+        <div className="min-h-screen sm:p-6 lg:p-8 w-full min-w-[320px] max-w-full">
             <title>Holidaze | Update venue</title>
-            <div className="max-w-2xl mx-auto">
-                <div className="flex justify-start mb-8">
+            <div className="max-w-md sm:max-w-lg lg:max-w-4xl mx-auto">
+                <div className="flex justify-start mb-4 sm:mb-6">
                     <button
                         onClick={handleBack}
-                        className="flex items-center gap-1 text-gray-900 text-sm font-semibold hover:underline">
+                        className="flex items-center gap-1 sm:gap-2 text-gray-900 text-sm sm:text-base font-semibold hover:underline">
                         <span>
                             <IoIosArrowBack />
-                        </span>{' '}
+                        </span>
                         Back to Venue
                     </button>
                 </div>
